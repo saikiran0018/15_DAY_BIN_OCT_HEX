d15d0b6b741fa96028068c07e96b70822cf55fef
saikiran0018 <109685878+saikiran0018@users.noreply.github.com>
2022-08-05 16:48:48 +0530

    Add files via upload

---
+++ bin_hex_oct.py
@@ -0,0 +1,32 @@
+print(bin(5))                                    #0b101
+print(0b111)                                     #7
+print(0B111)                                     #7
+
+
+
+
+print(oct(17))                                   #0o21
+print(0o21)                                      #17
+print(oct(24))                                   #0o30
+print(oct(6))                                    #0o6
+
+print(hex(18))                                   #0x12
+print(0x12)                                      #18
+print(hex(32))                                   #0x20
+
+
+
+print(0xA)                                         #10
+print(0xB)                                         #11
+print(0xC)                                         #12
+print(0xD)                                         #13   
+ 
+ 
+                                 
+print(0xE)                                         #14
+print(0xF)                                         #15
+print(0xG)                                         #syntaxerror
+
+
+a=0123
+print(a)                                           #syntaxerror
